0a8a47bbc0f0fd9d5550dbf258c6cbf763c871ef
alexMuhin <myx87@bk.ru>
2024-01-22 16:56:32 +0300

    refactoring project

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -1,164 +1,75 @@
-import React, {
-  useState,
-} from "react"
+import React, { useState } from "react"
 import Affairs from "./affairs/Affairs"
 import s2 from "../../s1-main/App.module.css"
 
 // types
-export type AffairPriorityType =
+export type AffairPriorityType = "high" | "low" | "middle"
+export type FilterType = "all" | AffairPriorityType
+export type AffairType = {
+  _id: number
+  name: string
+  priority: FilterType
+}
 
-    | "high"
-    | "low"
-    | "middle"
-export type FilterType =
-
-    | "all"
-    | AffairPriorityType
-export type AffairType =
+const defaultAffairs: AffairType[] = [
   {
-    _id: number
-    name: string
-    priority: FilterType
-  }
+    _id: 1,
+    name: "React",
+    priority: "high",
+  },
+  {
+    _id: 2,
+    name: "anime",
+    priority: "low",
+  },
+  {
+    _id: 3,
+    name: "games",
+    priority: "low",
+  },
+  {
+    _id: 4,
+    name: "work",
+    priority: "high",
+  },
+  {
+    _id: 5,
+    name: "html ",
+    priority: "middle",
+  },
+]
 
-const defaultAffairs: AffairType[] =
-  [
-    {
-      _id: 1,
-      name: "React",
-      priority:
-        "high",
-    },
-    {
-      _id: 2,
-      name: "anime",
-      priority:
-        "low",
-    },
-    {
-      _id: 3,
-      name: "games",
-      priority:
-        "low",
-    },
-    {
-      _id: 4,
-      name: "work",
-      priority:
-        "high",
-    },
-    {
-      _id: 5,
-      name: "html ",
-      priority:
-        "middle",
-    },
-  ]
-
-export const filterAffairs =
-  (
-    affairs: AffairType[],
-    filter: FilterType,
-  ): AffairType[] => {
-    if (
-      filter ===
-      "all"
-    ) {
-      return affairs
-    } else {
-      return affairs.filter(
-        (
-          affair,
-        ) =>
-          affair.priority ===
-          filter,
-      )
-    }
+export const filterAffairs = (affairs: AffairType[], filter: FilterType): AffairType[] => {
+  if (filter === "all") {
+    return affairs
+  } else {
+    return affairs.filter((affair) => affair.priority === filter)
   }
-export const deleteAffair =
-  (
-    affairs: AffairType[],
-    _id: number,
-  ): AffairType[] => {
-    return affairs.filter(
-      (
-        affair,
-      ) =>
-        affair._id !==
-        _id,
-    )
-  }
+}
+export const deleteAffair = (affairs: AffairType[], _id: number): AffairType[] => {
+  return affairs.filter((affair) => affair._id !== _id)
+}
 
 function HW2() {
-  const [
-    affairs,
-    setAffairs,
-  ] =
-    useState<
-      AffairType[]
-    >(
-      defaultAffairs,
-    )
-  const [
-    filter,
-    setFilter,
-  ] =
-    useState<FilterType>(
-      "all",
-    )
+  const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs)
+  const [filter, setFilter] = useState<FilterType>("all")
 
-  const filteredAffairs =
-    filterAffairs(
-      affairs,
-      filter,
-    )
+  const filteredAffairs = filterAffairs(affairs, filter)
 
-  const deleteAffairCallback =
-    (
-      _id: number,
-    ) => {
-      const filteredAffairs =
-        deleteAffair(
-          affairs,
-          _id,
-        )
-      setAffairs(
-        filteredAffairs,
-      )
-    }
+  const deleteAffairCallback = (_id: number) => {
+    const filteredAffairs = deleteAffair(affairs, _id)
+    setAffairs(filteredAffairs)
+  }
 
   return (
-    <div
-      id={
-        "hw2"
-      }
-    >
-      <div
-        className={
-          s2.hwTitle
-        }
-      >
-        Homework
-        #2
-      </div>
-      <div
-        className={
-          s2.hw
-        }
-      >
+    <div id={"hw2"}>
+      <div className={s2.hwTitle}>Homework #2</div>
+      <div className={s2.hw}>
         <Affairs
-          data={
-            filteredAffairs
-          }
-          setFilter={
-            setFilter
-          }
-          deleteAffairCallback={
-            deleteAffairCallback
-          }
-          filter={
-            filter
-          }
+          data={filteredAffairs}
+          setFilter={setFilter}
+          deleteAffairCallback={deleteAffairCallback}
+          filter={filter}
         />
       </div>
     </div>
