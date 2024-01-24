7adefb5d1027f5c1aef4369f6e91cddf1d81f554
alexMuhin <myx87@bk.ru>
2024-01-24 16:56:52 +0300

    refactoring project

--- src/s2-homeworks/hw09/HW9.tsx
+++ src/s2-homeworks/hw09/HW9.tsx
@@ -9,32 +9,16 @@ import s2 from "../../s1-main/App.module.css"
  * 4 - сделать стили в соответствии с дизайном
  * */
 
-const HW9 =
-  () => {
-    return (
-      <div
-        id={
-          "hw9"
-        }
-      >
-        <div
-          className={
-            s2.hwTitle
-          }
-        >
-          Homework
-          #9
-        </div>
+const HW9 = () => {
+  return (
+    <div id={"hw9"}>
+      <div className={s2.hwTitle}>Homework #9</div>
 
-        <div
-          className={
-            s2.hw
-          }
-        >
-          <Clock />
-        </div>
+      <div className={s2.hw}>
+        <Clock />
       </div>
-    )
-  }
+    </div>
+  )
+}
 
 export default HW9
