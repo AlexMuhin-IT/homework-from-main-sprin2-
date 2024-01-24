a4cdc5a37ea88ec4ffc9fc22449e50a85dd75e12
alexMuhin <myx87@bk.ru>
2024-01-24 16:56:55 +0300

    refactoring project

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,8 +1,5 @@
 import React from "react"
-import {
-  useDispatch,
-  useSelector,
-} from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { AppStoreType } from "./bll/store"
 import { loadingAC } from "./bll/loadingReducer"
 import SuperButton from "../hw04/common/c2-SuperButton/SuperButton"
@@ -16,90 +13,35 @@ import { Loader } from "./Loader"
  * 4 - сделать стили в соответствии с дизайном
  * */
 
-const HW10 =
-  () => {
-    const isLoading =
-      useSelector<
-        AppStoreType,
-        boolean
-      >(
-        (
-          state,
-        ) =>
-          state
-            .loading
-            .isLoading,
-      )
-    const dispatch =
-      useDispatch()
+const HW10 = () => {
+  const isLoading = useSelector<AppStoreType, boolean>((state) => state.loading.isLoading)
+  const dispatch = useDispatch()
 
-    const setLoading =
-      () => {
-        dispatch(
-          loadingAC(
-            true,
-          ),
-        )
+  const setLoading = () => {
+    dispatch(loadingAC(true))
 
-        setTimeout(
-          () => {
-            dispatch(
-              loadingAC(
-                false,
-              ),
-            )
-          },
-          1500,
-        )
-      }
-
-    return (
-      <div
-        id={
-          "hw10"
-        }
-      >
-        <div
-          className={
-            s2.hwTitle
-          }
-        >
-          Homework
-          #10
-        </div>
-
-        <div
-          className={
-            s2.hw
-          }
-        >
-          {isLoading ? (
-            <div
-              id={
-                "hw10-loading"
-              }
-            >
-              <Loader />
-            </div>
-          ) : (
-            <SuperButton
-              id={
-                "hw10-button-start-loading"
-              }
-              onClick={
-                setLoading
-              }
-              xType={
-                "default"
-              }
-            >
-              Set
-              loading...
-            </SuperButton>
-          )}
-        </div>
-      </div>
-    )
+    setTimeout(() => {
+      dispatch(loadingAC(false))
+    }, 1500)
   }
+
+  return (
+    <div id={"hw10"}>
+      <div className={s2.hwTitle}>Homework #10</div>
+
+      <div className={s2.hw}>
+        {isLoading ? (
+          <div id={"hw10-loading"}>
+            <Loader />
+          </div>
+        ) : (
+          <SuperButton id={"hw10-button-start-loading"} onClick={setLoading} xType={"default"}>
+            Set loading...
+          </SuperButton>
+        )}
+      </div>
+    </div>
+  )
+}
 
 export default HW10
