6e9df233e75a1527668ab0329cb6e9ad6b625149
alexMuhin <myx87@bk.ru>
2024-01-25 16:57:19 +0300

    refactoring project

--- src/index.tsx
+++ src/index.tsx
@@ -6,20 +6,11 @@ import reportWebVitals from "./reportWebVitals"
 import store from "./s2-homeworks/hw10/bll/store"
 import { Provider } from "react-redux"
 
-const root =
-  ReactDOM.createRoot(
-    document.getElementById(
-      "root",
-    ) as HTMLElement,
-  )
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     {/*для дз 10*/}
-    <Provider
-      store={
-        store
-      }
-    >
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
