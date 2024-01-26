fd768404802714a3dd57460a5d1058603497e505
alexMuhin <myx87@bk.ru>
2024-01-26 16:57:39 +0300

    refactoring project

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,40 +1,29 @@
-const initState =
-  {
-    isLoading:
-      false,
+const initState = {
+  isLoading: false,
+}
+
+export const loadingReducer = (state = initState, action: LoadingActionType): typeof initState => {
+  // fix any
+  switch (
+    action.type // пишет студент  // need to fix
+  ) {
+    case "CHANGE_LOADING":
+      return {
+        ...state,
+        isLoading: action.isLoading,
+      }
+
+    default:
+      return state
   }
+}
 
-export const loadingReducer =
-  (
-    state = initState,
-    action: LoadingActionType,
-  ): typeof initState => {
-    // fix any
-    switch (
-      action.type // пишет студент  // need to fix
-    ) {
-      case "CHANGE_LOADING":
-        return {
-          ...state,
-          isLoading:
-            action.isLoading,
-        }
+export const loadingAC = (isLoading: boolean): LoadingActionType => ({
+  type: "CHANGE_LOADING",
+  isLoading,
+})
 
-      default:
-        return state
-    }
-  }
-
-export const loadingAC =
-  (
-    isLoading: boolean,
-  ): LoadingActionType => ({
-    type: "CHANGE_LOADING",
-    isLoading,
-  })
-
-type LoadingActionType =
-  {
-    type: "CHANGE_LOADING"
-    isLoading: boolean
-  }
+type LoadingActionType = {
+  type: "CHANGE_LOADING"
+  isLoading: boolean
+}
