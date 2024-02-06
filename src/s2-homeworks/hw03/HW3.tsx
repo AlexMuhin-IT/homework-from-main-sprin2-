f013f42d6d2d653a367578399495b9f2e401d932
alexMuhin <myx87@bk.ru>
2024-02-06 17:01:10 +0300

    refactoring project

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -1,38 +1,92 @@
-import React, { useState } from "react"
+import React, {
+  useState,
+} from "react"
 import { v1 } from "uuid"
 import s2 from "../../s1-main/App.module.css"
 import GreetingContainer from "./GreetingContainer"
 
-export type UserType = {
-  _id: string
-  name: string
-}
-
-export const pureAddUserCallback = (name: string, setUsers: (u: UserType[]) => void, users: UserType[]) => {
-  const user: UserType = {
-    _id: v1(),
-    name: name,
-  }
-  setUsers([...users, user])
-}
-
-const HW3 = () => {
-  const [users, setUsers] = useState<UserType[]>([])
-
-  const addUserCallback = (name: string) => {
-    pureAddUserCallback(name, setUsers, users)
+export type UserType =
+  {
+    _id: string
+    name: string
   }
 
-  return (
-    <div id={"hw3"}>
-      <div className={s2.hwTitle}>Homework #3</div>
-      {/*для автоматической проверки дз (не менять)*/}
+export const pureAddUserCallback =
+  (
+    name: string,
+    setUsers: (
+      u: UserType[],
+    ) => void,
+    users: UserType[],
+  ) => {
+    const user: UserType =
+      {
+        _id: v1(),
+        name: name,
+      }
+    setUsers(
+      [
+        ...users,
+        user,
+      ],
+    )
+  }
 
-      <div className={s2.hw}>
-        <GreetingContainer users={users} addUserCallback={addUserCallback} />
+const HW3 =
+  () => {
+    const [
+      users,
+      setUsers,
+    ] =
+      useState<
+        UserType[]
+      >(
+        [],
+      )
+
+    const addUserCallback =
+      (
+        name: string,
+      ) => {
+        pureAddUserCallback(
+          name,
+          setUsers,
+          users,
+        )
+      }
+
+    return (
+      <div
+        id={
+          "hw3"
+        }
+      >
+        <div
+          className={
+            s2.hwTitle
+          }
+        >
+          Homework
+          #3
+        </div>
+        {/*для автоматической проверки дз (не менять)*/}
+
+        <div
+          className={
+            s2.hw
+          }
+        >
+          <GreetingContainer
+            users={
+              users
+            }
+            addUserCallback={
+              addUserCallback
+            }
+          />
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 
 export default HW3
