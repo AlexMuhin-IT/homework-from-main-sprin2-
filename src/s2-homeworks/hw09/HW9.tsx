73df44ad31e12975f29baaafb10d78cdf8384cca
alexMuhin <myx87@bk.ru>
2024-04-17 11:19:59 +0300

    refackor

--- src/s2-homeworks/hw09/HW9.tsx
+++ src/s2-homeworks/hw09/HW9.tsx
@@ -9,32 +9,26 @@ import s2 from "../../s1-main/App.module.css"
  * 4 - сделать стили в соответствии с дизайном
  * */
 
-const HW9 =
-  () => {
-    return (
+const HW9 = () => {
+  return (
+    <div id={"hw9"}>
       <div
-        id={
-          "hw9"
+        className={
+          s2.hwTitle
         }
       >
-        <div
-          className={
-            s2.hwTitle
-          }
-        >
-          Homework
-          #9
-        </div>
-
-        <div
-          className={
-            s2.hw
-          }
-        >
-          <Clock />
-        </div>
+        Homework #9
       </div>
-    )
-  }
+
+      <div
+        className={
+          s2.hw
+        }
+      >
+        <Clock />
+      </div>
+    </div>
+  )
+}
 
 export default HW9
