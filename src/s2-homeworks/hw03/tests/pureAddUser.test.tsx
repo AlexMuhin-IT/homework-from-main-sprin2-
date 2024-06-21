a3c5cd97582e43e2bf6b1e48f684ffd13b550e59
alexMuhin <myx87@bk.ru>
2024-06-21 11:21:14 +0300

    refackor

--- src/s2-homeworks/hw03/tests/pureAddUser.test.tsx
+++ src/s2-homeworks/hw03/tests/pureAddUser.test.tsx
@@ -2,76 +2,56 @@ import React from "react"
 import { pureAddUser } from "../GreetingContainer"
 
 let name: any
-const setName =
-  (
-    a: any,
-  ) => {
-    name =
-      a
-  }
+const setName = (
+  a: any,
+) => {
+  name = a
+}
 let error: any
-const setError =
-  (
-    a: any,
-  ) => {
-    error =
-      a
-  }
+const setError = (
+  a: any,
+) => {
+  error = a
+}
 let added: any
 const addUserCallback =
   () => {
-    added =
-      true
+    added = true
   }
 
-beforeEach(
-  () => {
-    name =
-      ""
-    error =
-      ""
-    added =
-      false
-  },
-)
+beforeEach(() => {
+  name = ""
+  error = ""
+  added = false
+})
 
 test("name 1", () => {
-  name =
-    "1"
+  name = "1"
   pureAddUser(
     name,
     setError,
     setName,
     addUserCallback,
   )
-  expect(
-    name,
-  ).toBe(
+  expect(name).toBe(
     "",
   )
   expect(
     error,
-  ).toBe(
-    "",
-  )
+  ).toBe("")
   expect(
     added,
-  ).toBe(
-    true,
-  )
+  ).toBe(true)
 })
 test("name 2", () => {
-  name =
-    ""
+  name = ""
   pureAddUser(
     name,
     setError,
     setName,
     addUserCallback,
   )
-  expect(
-    name,
-  ).toBe(
+  expect(name).toBe(
     "",
   )
   expect(
@@ -81,22 +61,17 @@ test("name 2", () => {
   )
   expect(
     added,
-  ).toBe(
-    false,
-  )
+  ).toBe(false)
 })
 test("name 3", () => {
-  name =
-    "    "
+  name = "    "
   pureAddUser(
     name,
     setError,
     setName,
     addUserCallback,
   )
-  expect(
-    name,
-  ).toBe(
+  expect(name).toBe(
     "    ",
   )
   expect(
@@ -106,7 +81,5 @@ test("name 3", () => {
   )
   expect(
     added,
-  ).toBe(
-    false,
-  )
+  ).toBe(false)
 })
