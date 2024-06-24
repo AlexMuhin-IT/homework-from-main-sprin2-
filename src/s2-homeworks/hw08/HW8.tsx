e155eadaa631c3866fc130ade017ee45cc3698c9
alexMuhin <myx87@bk.ru>
2024-06-24 11:21:43 +0300

    refackor

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -48,225 +48,211 @@ export const initialPeople: UserType[] =
     },
   ]
 
-const HW8 =
-  () => {
-    const [
-      people,
-      setPeople,
-    ] =
-      useState<
-        UserType[]
-      >(
-        initialPeople,
-      )
-    const [
-      currentSort,
-      setCurrentSort,
-    ] =
-      useState(
-        "",
-      )
+const HW8 = () => {
+  const [
+    people,
+    setPeople,
+  ] = useState<
+    UserType[]
+  >(initialPeople)
+  const [
+    currentSort,
+    setCurrentSort,
+  ] = useState("")
 
-    const finalPeople =
-      people.map(
-        (
-          u: UserType,
-        ) => (
-          <User
-            key={
-              u._id
-            }
-            u={
-              u
-            }
-          />
+  const finalPeople =
+    people.map(
+      (
+        u: UserType,
+      ) => (
+        <User
+          key={
+            u._id
+          }
+          u={u}
+        />
+      ),
+    )
+
+  const sortUp =
+    () => {
+      setPeople(
+        homeWorkReducer(
+          initialPeople,
+          {
+            type: "sort",
+            payload:
+              "up",
+          },
         ),
       )
+      setCurrentSort(
+        "up",
+      )
+    }
 
-    const sortUp =
-      () => {
-        setPeople(
-          homeWorkReducer(
-            initialPeople,
-            {
-              type: "sort",
-              payload:
-                "up",
-            },
-          ),
-        )
-        setCurrentSort(
-          "up",
-        )
-      }
+  const sortDown =
+    () => {
+      setPeople(
+        homeWorkReducer(
+          initialPeople,
+          {
+            type: "sort",
+            payload:
+              "down",
+          },
+        ),
+      )
+      setCurrentSort(
+        "down",
+      )
+    }
+  const check18 =
+    () => {
+      setPeople(
+        homeWorkReducer(
+          initialPeople,
+          {
+            type: "check",
+            payload: 18,
+          },
+        ),
+      )
+      setCurrentSort(
+        "18",
+      )
+    }
 
-    const sortDown =
-      () => {
-        setPeople(
-          homeWorkReducer(
-            initialPeople,
-            {
-              type: "sort",
-              payload:
-                "down",
-            },
-          ),
-        )
-        setCurrentSort(
-          "down",
-        )
-      }
-    const check18 =
-      () => {
-        setPeople(
-          homeWorkReducer(
-            initialPeople,
-            {
-              type: "check",
-              payload: 18,
-            },
-          ),
-        )
-        setCurrentSort(
-          "18",
-        )
-      }
-
-    return (
+  return (
+    <div id={"hw3"}>
       <div
-        id={
-          "hw3"
+        className={
+          s2.hwTitle
+        }
+      >
+        Homework #8
+      </div>
+      <div
+        className={
+          s2.hw
         }
       >
         <div
           className={
-            s2.hwTitle
-          }
-        >
-          Homework
-          #8
-        </div>
-        <div
-          className={
-            s2.hw
+            s.container
           }
         >
           <div
             className={
-              s.container
+              s.buttonsContainer
             }
           >
-            <div
-              className={
-                s.buttonsContainer
-              }
-            >
-              <SuperButton
-                id={
-                  "hw8-button-up"
-                }
-                onClick={
-                  sortUp
-                }
-                xType={
-                  currentSort ===
-                  "up"
-                    ? ""
-                    : "secondary"
-                }
-                disabled={
-                  currentSort ===
-                  "up"
-                }
-              >
-                Sort
-                up
-              </SuperButton>
-              <SuperButton
-                id={
-                  "hw8-button-down"
-                }
-                onClick={
-                  sortDown
-                }
-                xType={
-                  currentSort ===
-                  "down"
-                    ? ""
-                    : "secondary"
-                }
-                disabled={
-                  currentSort ===
-                  "down"
-                }
-              >
-                Sort
-                down
-              </SuperButton>
-              <SuperButton
-                id={
-                  "hw8-button-18"
-                }
-                onClick={
-                  check18
-                }
-                xType={
-                  currentSort ===
-                  "18"
-                    ? ""
-                    : "secondary"
-                }
-                disabled={
-                  currentSort ===
-                  "18"
-                }
-              >
-                Check
-                18+
-              </SuperButton>
-            </div>
-
-            <table
+            <SuperButton
               id={
-                "hw8-users"
+                "hw8-button-up"
               }
-              className={
-                s.users
+              onClick={
+                sortUp
+              }
+              xType={
+                currentSort ===
+                "up"
+                  ? ""
+                  : "secondary"
+              }
+              disabled={
+                currentSort ===
+                "up"
               }
             >
-              <thead
-                className={
-                  s.thead
-                }
-              >
-                <tr>
-                  <td
-                    className={
-                      s.nameCol
-                    }
-                  >
-                    Name
-                  </td>
-                  <td
-                    className={
-                      s.ageCol
-                    }
-                  >
-                    Age
-                  </td>
-                </tr>
-              </thead>
-
-              <tbody>
-                {
-                  finalPeople
-                }
-              </tbody>
-            </table>
+              Sort
+              up
+            </SuperButton>
+            <SuperButton
+              id={
+                "hw8-button-down"
+              }
+              onClick={
+                sortDown
+              }
+              xType={
+                currentSort ===
+                "down"
+                  ? ""
+                  : "secondary"
+              }
+              disabled={
+                currentSort ===
+                "down"
+              }
+            >
+              Sort
+              down
+            </SuperButton>
+            <SuperButton
+              id={
+                "hw8-button-18"
+              }
+              onClick={
+                check18
+              }
+              xType={
+                currentSort ===
+                "18"
+                  ? ""
+                  : "secondary"
+              }
+              disabled={
+                currentSort ===
+                "18"
+              }
+            >
+              Check
+              18+
+            </SuperButton>
           </div>
+
+          <table
+            id={
+              "hw8-users"
+            }
+            className={
+              s.users
+            }
+          >
+            <thead
+              className={
+                s.thead
+              }
+            >
+              <tr>
+                <td
+                  className={
+                    s.nameCol
+                  }
+                >
+                  Name
+                </td>
+                <td
+                  className={
+                    s.ageCol
+                  }
+                >
+                  Age
+                </td>
+              </tr>
+            </thead>
+
+            <tbody>
+              {
+                finalPeople
+              }
+            </tbody>
+          </table>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 
 export default HW8
