c6a5327dcd73a7badd10485efaa8bf5f3251d2c0
alexMuhin <myx87@bk.ru>
2024-07-17 16:43:06 +0300

    refactoring project

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -1,13 +1,8 @@
-import React, {
-  useEffect,
-} from "react"
+import React, { useEffect } from "react"
 import s from "./HW12.module.css"
 import s2 from "../../s1-main/App.module.css"
 import SuperSelect from "../hw07/common/c5-SuperSelect/SuperSelect"
-import {
-  useDispatch,
-  useSelector,
-} from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { changeThemeId } from "./bll/themeReducer"
 import { AppStoreType } from "../hw10/bll/store"
 
@@ -18,89 +13,46 @@ import { AppStoreType } from "../hw10/bll/store"
  * 4 - передать пропсы в SuperSelect
  * */
 
-export const themes =
-  [
-    {
-      id: 1,
-      value:
-        "light",
-    },
-    {
-      id: 2,
-      value: "blue",
-    },
-    {
-      id: 3,
-      value: "dark",
-    },
-  ]
+export const themes = [
+  {
+    id: 1,
+    value: "light",
+  },
+  {
+    id: 2,
+    value: "blue",
+  },
+  {
+    id: 3,
+    value: "dark",
+  },
+]
 
 const HW12 = () => {
-  const themeId =
-    useSelector<
-      AppStoreType,
-      number
-    >(
-      (state) =>
-        state.theme
-          .themeId,
-    )
-  const dispatch =
-    useDispatch()
-  console.log(
-    themeId,
-  )
-  const change = (
-    id: any,
-  ) => {
-    dispatch(
-      changeThemeId(
-        id,
-      ),
-    )
+  const themeId = useSelector<AppStoreType, number>((state) => state.theme.themeId)
+  const dispatch = useDispatch()
+  console.log(themeId)
+  const change = (id: any) => {
+    dispatch(changeThemeId(id))
   }
 
   useEffect(() => {
-    document.documentElement.dataset.theme =
-      themeId + ""
+    document.documentElement.dataset.theme = themeId + ""
   }, [themeId])
 
   return (
-    <div
-      id={"hw12"}
-    >
-      <div
-        id={
-          "hw12-text"
-        }
-        className={
-          s2.hwTitle
-        }
-      >
+    <div id={"hw12"}>
+      <div id={"hw12-text"} className={s2.hwTitle}>
         Homework #12
       </div>
 
-      <div
-        className={
-          s2.hw
-        }
-      >
+      <div className={s2.hw}>
         <SuperSelect
-          id={
-            "hw12-select-theme"
-          }
-          className={
-            s.select
-          }
-          value={
-            themeId
-          }
-          onChangeOption={
-            change
-          }
-          options={
-            themes
-          }
+          id={"hw12-select-theme"}
+          className={s.select}
+          value={themeId}
+          onChangeOption={change}
+          options={themes}
         />
       </div>
     </div>
