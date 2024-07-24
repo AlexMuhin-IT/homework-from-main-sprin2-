98f157bc72e30dbf135d06b6d794eb3eecf19fc4
alexMuhin <myx87@bk.ru>
2024-07-24 16:44:46 +0300

    refactoring project

--- src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
+++ src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
@@ -1,36 +1,16 @@
-import React, {
-  ChangeEvent,
-  DetailedHTMLProps,
-  InputHTMLAttributes,
-  KeyboardEvent,
-  ReactNode,
-} from "react"
+import React, { ChangeEvent, DetailedHTMLProps, InputHTMLAttributes, KeyboardEvent, ReactNode } from "react"
 import s from "./SuperInputText.module.css"
 
-type DefaultInputPropsType =
-  DetailedHTMLProps<
-    InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
-  >
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
-type SuperInputTextPropsType =
-  Omit<
-    DefaultInputPropsType,
-    "type"
-  > & {
-    onChangeText?: (
-      value: string,
-    ) => void
-    onEnter?: (
-      e: KeyboardEvent<HTMLInputElement>,
-    ) => void
-    error?: ReactNode
-    spanClassName?: string
-  }
+type SuperInputTextPropsType = Omit<DefaultInputPropsType, "type"> & {
+  onChangeText?: (value: string) => void
+  onEnter?: (e: KeyboardEvent<HTMLInputElement>) => void
+  error?: ReactNode
+  spanClassName?: string
+}
 
-const SuperInputText: React.FC<
-  SuperInputTextPropsType
-> = ({
+const SuperInputText: React.FC<SuperInputTextPropsType> = ({
   onChange,
   onChangeText,
   onKeyPress,
@@ -42,33 +22,15 @@ const SuperInputText: React.FC<
 
   ...restProps
 }) => {
-  const onChangeCallback =
-    (
-      e: ChangeEvent<HTMLInputElement>,
-    ) => {
-      onChange &&
-        onChange(e)
-      onChangeText &&
-        onChangeText(
-          e
-            .currentTarget
-            .value,
-        )
-    }
-  const onKeyPressCallback =
-    (
-      e: KeyboardEvent<HTMLInputElement>,
-    ) => {
-      onKeyPress &&
-        onKeyPress(
-          e,
-        )
+  const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange && onChange(e)
+    onChangeText && onChangeText(e.currentTarget.value)
+  }
+  const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
+    onKeyPress && onKeyPress(e)
 
-      onEnter &&
-        e.key ===
-          "Enter" &&
-        onEnter(e)
-    }
+    onEnter && e.key === "Enter" && onEnter(e)
+  }
 
   const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ""}`
 
@@ -76,38 +38,16 @@ const SuperInputText: React.FC<
     ${error ? s.errorInput : s.superInput} 
     ${className ? s.input + s.className : s.input} `
   return (
-    <div
-      className={
-        s.inputWrapper
-      }
-    >
+    <div className={s.inputWrapper}>
       <input
         id={id}
-        type={
-          "text"
-        }
-        onChange={
-          onChangeCallback
-        }
-        onKeyPress={
-          onKeyPressCallback
-        }
-        className={
-          finalInputClassName
-        }
+        type={"text"}
+        onChange={onChangeCallback}
+        onKeyPress={onKeyPressCallback}
+        className={finalInputClassName}
         {...restProps}
       />
-      <span
-        id={
-          id
-            ? id +
-              "-span"
-            : undefined
-        }
-        className={
-          finalSpanClassName
-        }
-      >
+      <span id={id ? id + "-span" : undefined} className={finalSpanClassName}>
         {error}
       </span>
     </div>
