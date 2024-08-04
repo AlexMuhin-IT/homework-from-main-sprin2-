0c6e1112983afaf39026f617fa9ac53f703b184b
alexMuhin <myx87@bk.ru>
2024-08-04 15:58:50 +0300

    refactoring project

--- src/s2-homeworks/hw05/HW5.tsx
+++ src/s2-homeworks/hw05/HW5.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import {HashRouter} from 'react-router-dom'
-import {Layout} from './layout/Layout'
-import Pages from './Pages'
-import HW14 from "../hw14/HW14";
+import React from "react"
+import { HashRouter } from "react-router-dom"
+import { Layout } from "./layout/Layout"
+import Pages from "./Pages"
+import HW14 from "../hw14/HW14"
 
 function HW5() {
-	return (
-		<HashRouter>
-			<Layout>
-				<Pages/>
-			</Layout>
-		</HashRouter>
-	)
+  return (
+    <HashRouter>
+      <Layout>
+        <Pages />
+      </Layout>
+    </HashRouter>
+  )
 }
 
 export default HW5
