18a6570b8558f1085737bd04aa8946976fd96877
alexMuhin <myx87@bk.ru>
2024-08-04 15:58:40 +0300

    refactoring project

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import s2 from '../../s1-main/App.module.css'
-import Stand from './Stand'
-
+import React from "react"
+import s2 from "../../s1-main/App.module.css"
+import Stand from "./Stand"
 
 const HW4 = () => {
-    return (
-        <div id={'hw4'}>
-            <div className={s2.hwTitle}>Homework #4</div>
-            {/*демонстрация возможностей компонент:*/}
-            <div className={s2.hw}>
-                <Stand />
-            </div>
-        </div>
-    )
+  return (
+    <div id={"hw4"}>
+      <div
+        className={
+          s2.hwTitle
+        }
+      >
+        Homework #4
+      </div>
+      {/*демонстрация возможностей компонент:*/}
+      <div
+        className={
+          s2.hw
+        }
+      >
+        <Stand />
+      </div>
+    </div>
+  )
 }
 
 export default HW4
