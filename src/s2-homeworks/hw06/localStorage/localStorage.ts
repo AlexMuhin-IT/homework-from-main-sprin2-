f4350bbdfa41214472e26839e4870d1241b0f5ad
alexMuhin <myx87@bk.ru>
2024-08-13 15:59:40 +0300

    refactoring project

--- src/s2-homeworks/hw06/localStorage/localStorage.ts
+++ src/s2-homeworks/hw06/localStorage/localStorage.ts
@@ -1,16 +1,43 @@
 // вот вам функция для сохранения объектов в память браузера
 // (данные в этом хранилище сохраняться даже при перезагрузке компа):
-export function saveState<T>(key: string, state: T) {
-    const stateAsString = JSON.stringify(state)
-    localStorage.setItem(key, stateAsString)
+export function saveState<
+  T,
+>(
+  key: string,
+  state: T,
+) {
+  const stateAsString =
+    JSON.stringify(
+      state,
+    )
+  localStorage.setItem(
+    key,
+    stateAsString,
+  )
 }
 
 // и вот вам функция для получения сохранённого объекта в памяти браузера:
-export function restoreState<T>(key: string, defaultState: T) {
-    let state = defaultState
-    const stateAsString = localStorage.getItem(key)
-    if (stateAsString !== null) state = JSON.parse(stateAsString) as T
-    return state
+export function restoreState<
+  T,
+>(
+  key: string,
+  defaultState: T,
+) {
+  let state =
+    defaultState
+  const stateAsString =
+    localStorage.getItem(
+      key,
+    )
+  if (
+    stateAsString !==
+    null
+  )
+    state =
+      JSON.parse(
+        stateAsString,
+      ) as T
+  return state
 }
 
 // ---------------------------------------------------------------------------------------------------------------
@@ -28,4 +55,3 @@ export function restoreState<T>(key: string, defaultState: T) {
 //
 // // получаем в переменную state объект из ячейки 'test' или дэфолтный объект если ячейка пуста
 // const state: StateType = restoreState<StateType>('test', { x: '', y: 0 })
-
