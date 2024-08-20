2266addd86574898505db023c7ed76039fb36b8a
alexMuhin <myx87@bk.ru>
2024-08-20 16:01:01 +0300

    refactoring project

--- src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
+++ src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import {pureAddUserCallback} from '../HW3'
+import React from "react"
+import { pureAddUserCallback } from "../HW3"
 
 let initialState: any[]
-const setName = (a: any[]) => {
-    initialState = a
+const setName = (
+  a: any[],
+) => {
+  initialState = a
 }
 
 beforeEach(() => {
-    initialState = []
+  initialState = []
 })
 
-test('name 1', () => {
-    pureAddUserCallback('name', setName, initialState)
-    expect(initialState.length).toBe(1)
-    expect(initialState[0].name).toBe('name')
-    expect(!!initialState[0]._id).toBe(true)
+test("name 1", () => {
+  pureAddUserCallback(
+    "name",
+    setName,
+    initialState,
+  )
+  expect(
+    initialState.length,
+  ).toBe(1)
+  expect(
+    initialState[0]
+      .name,
+  ).toBe("name")
+  expect(
+    !!initialState[0]
+      ._id,
+  ).toBe(true)
 })
