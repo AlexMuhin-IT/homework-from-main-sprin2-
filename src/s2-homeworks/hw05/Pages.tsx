a0e3249fe9f7dc51aa7ce7e82a4ad66ad6272005
alexMuhin <myx87@bk.ru>
2024-08-20 16:00:55 +0300

    refactoring project

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,34 +1,78 @@
-import React from 'react'
-import {Routes, Route, Navigate, NavLink} from 'react-router-dom'
-import Error404 from './pages/Error404'
-import PreJunior from './pages/PreJunior'
-import Junior from './pages/Junior'
-import JuniorPlus from './pages/JuniorPlus'
+import React from "react"
+import {
+  Routes,
+  Route,
+  Navigate,
+  NavLink,
+} from "react-router-dom"
+import Error404 from "./pages/Error404"
+import PreJunior from "./pages/PreJunior"
+import Junior from "./pages/Junior"
+import JuniorPlus from "./pages/JuniorPlus"
 
-
-export const PATH = {
-    PRE_JUNIOR: '/pre-junior',
-    JUNIOR: '/junior',
-    JUNIOR_PLUS: '/junior-plus',
-    ERROR404: '/error404'
-}
+export const PATH =
+  {
+    PRE_JUNIOR:
+      "/pre-junior",
+    JUNIOR:
+      "/junior",
+    JUNIOR_PLUS:
+      "/junior-plus",
+    ERROR404:
+      "/error404",
+  }
 
 function Pages() {
-    return (
-        <div>
-            <div>
-                <Routes>
-                    <Route path='/' element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+  return (
+    <div>
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Navigate
+                to={
+                  PATH.PRE_JUNIOR
+                }
+              />
+            }
+          />
 
-                    <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
-                    <Route path={PATH.JUNIOR} element={<Junior/>}/>
-                    <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
+          <Route
+            path={
+              PATH.PRE_JUNIOR
+            }
+            element={
+              <PreJunior />
+            }
+          />
+          <Route
+            path={
+              PATH.JUNIOR
+            }
+            element={
+              <Junior />
+            }
+          />
+          <Route
+            path={
+              PATH.JUNIOR_PLUS
+            }
+            element={
+              <JuniorPlus />
+            }
+          />
 
-                    <Route path='*' element={<Error404/>}/>
-                </Routes>
-            </div>
-        </div>
-    )
+          <Route
+            path="*"
+            element={
+              <Error404 />
+            }
+          />
+        </Routes>
+      </div>
+    </div>
+  )
 }
 
 export default Pages
