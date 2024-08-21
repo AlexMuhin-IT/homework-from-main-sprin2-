2f047b5919f480e61b37d343ad8216203c15dfff
alexMuhin <myx87@bk.ru>
2024-08-21 16:07:34 +0300

    refactoring project

--- src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
+++ src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
@@ -1,68 +1,132 @@
-import React from 'react'
-import SuperSelect from '../../../hw07/common/c5-SuperSelect/SuperSelect'
-import {Pagination} from '@mui/material'
-import s from './SuperPagination.module.css'
+import React from "react"
+import SuperSelect from "../../../hw07/common/c5-SuperSelect/SuperSelect"
+import { Pagination } from "@mui/material"
+import s from "./SuperPagination.module.css"
 
-export type SuperPaginationPropsType = {
-	id?: string
-	page: number
-	itemsCountForPage: number
-	totalCount: number
-	onChange: (page: number, count: number) => void
-}
+export type SuperPaginationPropsType =
+  {
+    id?: string
+    page: number
+    itemsCountForPage: number
+    totalCount: number
+    onChange: (
+      page: number,
+      count: number,
+    ) => void
+  }
 
-const SuperPagination: React.FC<SuperPaginationPropsType> = (
-	{
-		page, itemsCountForPage, totalCount, onChange, id = 'hw15',
-	}
-) => {
+const SuperPagination: React.FC<
+  SuperPaginationPropsType
+> = ({
+  page,
+  itemsCountForPage,
+  totalCount,
+  onChange,
+  id = "hw15",
+}) => {
+  const lastPage =
+    Math.ceil(
+      totalCount /
+        itemsCountForPage,
+    )
 
-	const lastPage = Math.ceil(totalCount / itemsCountForPage)
+  const onChangeCallback =
+    (
+      event: React.ChangeEvent<unknown>,
+      page: number,
+    ) => {
+      onChange(
+        page,
+        itemsCountForPage,
+      )
+    }
 
-	const onChangeCallback = (event: React.ChangeEvent<unknown>, page: number) => {
-		onChange(page, itemsCountForPage)
-	}
+  const onChangeSelect =
+    (
+      event: React.ChangeEvent<HTMLSelectElement>,
+    ) => {
+      const currentEvent =
+        +event
+          .target
+          .value
+      onChange(
+        1,
+        currentEvent,
+      )
+    }
 
-	const onChangeSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
-		const currentEvent = +event.target.value
-		onChange(1, currentEvent)
+  return (
+    <div
+      className={
+        s.pagination
+      }
+    >
+      <Pagination
+        id={
+          id +
+          "-pagination"
+        }
+        sx={
+          {
+            // стили для Pagination // пишет студент
+          }
+        }
+        page={page}
+        count={
+          lastPage
+        }
+        onChange={
+          onChangeCallback
+        }
+        hideNextButton
+        hidePrevButton
+      />
 
-	}
+      <span
+        className={
+          s.text1
+        }
+      >
+        показать
+      </span>
 
-	return (
-		<div className={s.pagination}>
-			<Pagination
-				id={id + '-pagination'}
-				sx={{
-					// стили для Pagination // пишет студент
-				}}
-				page={page}
-				count={lastPage}
-				onChange={onChangeCallback}
-				hideNextButton
-				hidePrevButton
-			/>
+      <SuperSelect
+        id={
+          id +
+          "-pagination-select"
+        }
+        value={
+          itemsCountForPage
+        }
+        options={[
+          {
+            id: 4,
+            value: 4,
+          },
+          {
+            id: 7,
+            value: 7,
+          },
+          {
+            id: 10,
+            value: 10,
+          },
+        ]}
+        onChange={
+          onChangeSelect
+        }
+      />
 
-			<span className={s.text1}>
-                показать
-            </span>
-
-			<SuperSelect
-				id={id + '-pagination-select'}
-				value={itemsCountForPage}
-				options={[
-					{id: 4, value: 4},
-					{id: 7, value: 7},
-					{id: 10, value: 10},
-				]}
-				onChange={onChangeSelect}
-			/>
-
-			<span className={s.text2}>
-                строк в таблице
-            </span>
-		</div>
-	)
+      <span
+        className={
+          s.text2
+        }
+      >
+        строк в
+        таблице
+      </span>
+    </div>
+  )
 }
 
 export default SuperPagination
