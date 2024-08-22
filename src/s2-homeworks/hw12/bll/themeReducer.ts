7496f903fd8b0c5c88c7194fd9677da27d6648e7
alexMuhin <myx87@bk.ru>
2024-08-22 16:07:52 +0300

    refactoring project

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,21 +1,41 @@
-import {homeWorkReducer} from "../../hw08/bll/homeWorkReducer";
-type InitialState = typeof initState;
+import { homeWorkReducer } from "../../hw08/bll/homeWorkReducer"
+type InitialState =
+  typeof initState
 
 const initState = {
-    themeId: 1,
+  themeId: 1,
 }
 
-export const themeReducer = (state: InitialState = initState, action: ActionType): InitialState => {
-    switch (action.type) {
-        // дописать
-        case 'SET_THEME_ID':
-            return {...state, themeId: action.id}
-        default:
-            return state
+export const themeReducer =
+  (
+    state: InitialState = initState,
+    action: ActionType,
+  ): InitialState => {
+    switch (
+      action.type
+    ) {
+      // дописать
+      case "SET_THEME_ID":
+        return {
+          ...state,
+          themeId:
+            action.id,
+        }
+      default:
+        return state
     }
-}
+  }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }as const)
+export const changeThemeId =
+  (
+    id: number,
+  ): any =>
+    ({
+      type: "SET_THEME_ID",
+      id,
+    }) as const
 
-
-type ActionType = ReturnType<typeof changeThemeId>
+type ActionType =
+  ReturnType<
+    typeof changeThemeId
+  >
