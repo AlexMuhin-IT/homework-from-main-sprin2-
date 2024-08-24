9158fb28763ae6d1c7dec2e9f24ae1b17545f436
alexMuhin <myx87@bk.ru>
2024-08-24 16:04:11 +0300

    refactoring project

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -1,39 +1,50 @@
-import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
-import s from './SuperButton.module.css'
+import React, {
+  ButtonHTMLAttributes,
+  DetailedHTMLProps,
+} from "react"
+import s from "./SuperButton.module.css"
 
+type DefaultButtonPropsType =
+  DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >
 
-type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement>
-
-type SuperButtonPropsType = DefaultButtonPropsType & {
+type SuperButtonPropsType =
+  DefaultButtonPropsType & {
     xType?: string
-}
+  }
 
-const SuperButton: React.FC<SuperButtonPropsType> = (
-    {
-        xType,
-        className,
-        disabled,
-        ...restProps
-    }
-) => {
+const SuperButton: React.FC<
+  SuperButtonPropsType
+> = ({
+  xType,
+  className,
+  disabled,
+  ...restProps
+}) => {
+  const finalClassName = `${s.button} + 
+    ${xType === "default" ? s.default : ""}
+    ${disabled ? s.disabled + s.action : ""}
+    ${xType === "red" ? s.red : ""}
+    ${className ? " " + className : ""}
+    ${xType === "secondary" ? s.secondary : ""}`
 
-
-    const finalClassName = `${s.button} + 
-    ${xType === 'default' ? s.default : ''}
-    ${disabled ? s.disabled + s.action : ''}
-    ${xType === 'red' ? s.red : ''}
-    ${className ? ' ' + className : ''}
-    ${xType === 'secondary' ? s.secondary : ''}`;
-
-    return (
-        <button
-            disabled={disabled}
-            className={finalClassName}
-            {...restProps}
-
-        >{restProps.children}</button>
-    )
+  return (
+    <button
+      disabled={
+        disabled
+      }
+      className={
+        finalClassName
+      }
+      {...restProps}
+    >
+      {
+        restProps.children
+      }
+    </button>
+  )
 }
 
 export default SuperButton
