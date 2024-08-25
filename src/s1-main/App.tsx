95c1b1111d863ddd78c04f7c45414c78b71f5fe5
alexMuhin <myx87@bk.ru>
2024-08-25 16:09:18 +0300

    refactoring project

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -9,11 +9,7 @@ import HW6 from "../s2-homeworks/hw06/HW6"
 
 function App() {
   return (
-    <div
-      className={
-        s.App
-      }
-    >
+    <div className={s.App}>
       {/*<HW1/>*/}
       {/*<HW2/>*/}
       {/*<HW3/>*/}
