96ecf7cf3b0e10b1520bf8824a9bd818190820ae
alexMuhin <myx87@bk.ru>
2024-09-09 16:09:48 +0300

    refactoring project

--- src/s2-homeworks/hw05/pages/Error404.tsx
+++ src/s2-homeworks/hw05/pages/Error404.tsx
@@ -4,36 +4,14 @@ import error404 from "./404.svg"
 import { NavLink } from "react-router-dom"
 import { PATH } from "../Pages"
 
-const Error404 =
-  () => {
-    return (
-      <NavLink
-        to={
-          PATH.ERROR404
-        }
-        id={
-          "hw5-page-404"
-        }
-      >
-        <div
-          className={
-            s.wrapper
-          }
-        >
-          <img
-            src={
-              error404
-            }
-            alt={
-              "404"
-            }
-            className={
-              s.error404
-            }
-          />
-        </div>
-      </NavLink>
-    )
-  }
+const Error404 = () => {
+  return (
+    <NavLink to={PATH.ERROR404} id={"hw5-page-404"}>
+      <div className={s.wrapper}>
+        <img src={error404} alt={"404"} className={s.error404} />
+      </div>
+    </NavLink>
+  )
+}
 
 export default Error404
