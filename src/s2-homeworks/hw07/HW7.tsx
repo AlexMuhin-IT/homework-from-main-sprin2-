37b52b501b77411720146e486b8849a1e2452345
alexMuhin <myx87@bk.ru>
2024-09-13 16:10:33 +0300

    refactoring project

--- src/s2-homeworks/hw07/HW7.tsx
+++ src/s2-homeworks/hw07/HW7.tsx
@@ -1,6 +1,4 @@
-import React, {
-  useState,
-} from "react"
+import React, { useState } from "react"
 import SuperSelect from "./common/c5-SuperSelect/SuperSelect"
 import SuperRadio from "./common/c6-SuperRadio/SuperRadio"
 import s2 from "../../s1-main/App.module.css"
@@ -9,8 +7,7 @@ import s from "./HW7.module.css"
 const arr = [
   {
     id: 1,
-    value:
-      "Pre-junior",
+    value: "Pre-junior",
   },
   {
     id: 2,
@@ -18,71 +15,30 @@ const arr = [
   },
   {
     id: 3,
-    value:
-      "Junior +",
+    value: "Junior +",
   },
 ]
 
 const HW7 = () => {
-  const [
-    value,
-    onChangeOption,
-  ] = useState(1)
+  const [value, onChangeOption] = useState(1)
 
   return (
     <div id={"hw7"}>
-      <div
-        className={
-          s2.hwTitle
-        }
-      >
-        Homework #7
-      </div>
+      <div className={s2.hwTitle}>Homework #7</div>
 
       {/*демонстрация возможностей компонент:*/}
-      <div
-        className={
-          s2.hw
-        }
-      >
-        <div
-          className={
-            s.container
-          }
-        >
+      <div className={s2.hw}>
+        <div className={s.container}>
           <div>
-            <SuperSelect
-              id={
-                "hw7-super-select"
-              }
-              options={
-                arr
-              }
-              value={
-                value
-              }
-              onChangeOption={
-                onChangeOption
-              }
-            />
+            <SuperSelect id={"hw7-super-select"} options={arr} value={value} onChangeOption={onChangeOption} />
           </div>
           <div>
             <SuperRadio
-              id={
-                "hw7-super-radio"
-              }
-              name={
-                "hw7-radio"
-              }
-              options={
-                arr
-              }
-              value={
-                value
-              }
-              onChangeOption={
-                onChangeOption
-              }
+              id={"hw7-super-radio"}
+              name={"hw7-radio"}
+              options={arr}
+              value={value}
+              onChangeOption={onChangeOption}
             />
           </div>
         </div>
