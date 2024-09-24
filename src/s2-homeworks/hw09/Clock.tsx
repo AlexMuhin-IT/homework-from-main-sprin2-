0c06fb33c86717b543a03869e075b5241f574b58
alexMuhin <myx87@bk.ru>
2024-09-24 16:17:39 +0300

    refactoring project

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -1,78 +1,236 @@
-import React, { useState } from "react"
+import React, {
+  useState,
+} from "react"
 import SuperButton from "../hw04/common/c2-SuperButton/SuperButton"
 import { restoreState } from "../hw06/localStorage/localStorage"
 import s from "./Clock.module.css"
 
 function Clock() {
-  const [timerId, setTimerId] = useState<number | undefined>(undefined)
-  const [date, setDate] = useState<Date>(new Date(restoreState("hw9-date", Date.now())))
-  const [show, setShow] = useState<boolean>(false)
+  const [
+    timerId,
+    setTimerId,
+  ] = useState<
+    | number
+    | undefined
+  >(undefined)
+  const [
+    date,
+    setDate,
+  ] =
+    useState<Date>(
+      new Date(
+        restoreState(
+          "hw9-date",
+          Date.now(),
+        ),
+      ),
+    )
+  const [
+    show,
+    setShow,
+  ] =
+    useState<boolean>(
+      false,
+    )
 
-  const [disable, setDisable] = useState<boolean>(false) // ????
+  const [
+    disable,
+    setDisable,
+  ] =
+    useState<boolean>(
+      false,
+    ) // ????
 
-  const start = () => {
-    // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
+  const start =
+    () => {
+      // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
 
-    stop()
-    setDisable(!disable)
-    const id: number = window.setInterval(() => {
-      setDate(new Date())
-      setTimerId(id)
-    }, 1000)
-  }
+      stop()
+      setDisable(
+        !disable,
+      )
+      const id: number =
+        window.setInterval(
+          () => {
+            setDate(
+              new Date(),
+            )
+            setTimerId(
+              id,
+            )
+          },
+          1000,
+        )
+    }
 
-  const stop = () => {
-    clearInterval(timerId)
-    setDisable(!disable)
-  }
+  const stop =
+    () => {
+      clearInterval(
+        timerId,
+      )
+      setDisable(
+        !disable,
+      )
+    }
 
-  const onMouseEnter = () => {
-    setShow(true)
-  }
-  const onMouseLeave = () => {
-    setShow(false)
-  }
-  let formatterTime = new Intl.DateTimeFormat("ru", {
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-  })
-  let currentTime = formatterTime.format(date)
+  const onMouseEnter =
+    () => {
+      setShow(true)
+    }
+  const onMouseLeave =
+    () => {
+      setShow(false)
+    }
+  let formatterTime =
+    new Intl.DateTimeFormat(
+      "ru",
+      {
+        hour: "numeric",
+        minute:
+          "numeric",
+        second:
+          "numeric",
+      },
+    )
+  let currentTime =
+    formatterTime.format(
+      date,
+    )
 
-  let formatterDate = new Intl.DateTimeFormat("ru")
-  let currentDate = formatterDate.format(date)
+  let formatterDate =
+    new Intl.DateTimeFormat(
+      "ru",
+    )
+  let currentDate =
+    formatterDate.format(
+      date,
+    )
 
-  let formatterMonth = new Intl.DateTimeFormat("en-US", {
-    month: "long",
-  })
-  let currentMonth = formatterMonth.format(date)
+  let formatterMonth =
+    new Intl.DateTimeFormat(
+      "en-US",
+      {
+        month:
+          "long",
+      },
+    )
+  let currentMonth =
+    formatterMonth.format(
+      date,
+    )
 
-  let formatterDay = new Intl.DateTimeFormat("en-US", {
-    weekday: "long",
-  })
-  let currentDay = formatterDay.format(date)
+  let formatterDay =
+    new Intl.DateTimeFormat(
+      "en-US",
+      {
+        weekday:
+          "long",
+      },
+    )
+  let currentDay =
+    formatterDay.format(
+      date,
+    )
 
-  const stringTime = currentTime || <br /> // часы24:минуты:секунды (24:00:00)
-  const stringDate = currentDate || <br /> // день.месяц.год (01.02.2022)
+  const stringTime =
+    currentTime || (
+      <br />
+    ) // часы24:минуты:секунды (24:00:00)
+  const stringDate =
+    currentDate || (
+      <br />
+    ) // день.месяц.год (01.02.2022)
 
   // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-  const stringDay = currentDay || <br />
-  const stringMonth = currentMonth || <br />
+  const stringDay =
+    currentDay || (
+      <br />
+    )
+  const stringMonth =
+    currentMonth || (
+      <br />
+    )
 
   return (
-    <div className={s.clock}>
-      <div id={"hw9-watch"} className={s.watch} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-        <span id={"hw9-day"}>{stringDay}</span>,{" "}
-        <span id={"hw9-time"}>
-          <strong onBlur={(event) => onMouseEnter()}>{stringTime}</strong>
+    <div
+      className={
+        s.clock
+      }
+    >
+      <div
+        id={
+          "hw9-watch"
+        }
+        className={
+          s.watch
+        }
+        onMouseEnter={
+          onMouseEnter
+        }
+        onMouseLeave={
+          onMouseLeave
+        }
+      >
+        <span
+          id={
+            "hw9-day"
+          }
+        >
+          {
+            stringDay
+          }
+        </span>
+        ,{" "}
+        <span
+          id={
+            "hw9-time"
+          }
+        >
+          <strong
+            onBlur={(
+              event,
+            ) =>
+              onMouseEnter()
+            }
+          >
+            {
+              stringTime
+            }
+          </strong>
         </span>
       </div>
 
-      <div id={"hw9-more"}>
-        <div className={s.more}>
+      <div
+        id={
+          "hw9-more"
+        }
+      >
+        <div
+          className={
+            s.more
+          }
+        >
           {show ? (
             <>
-              <span id={"hw9-month"}>{stringMonth}</span>, <span id={"hw9-date"}>{stringDate}</span>
+              <span
+                id={
+                  "hw9-month"
+                }
+              >
+                {
+                  stringMonth
+                }
+              </span>
+              ,{" "}
+              <span
+                id={
+                  "hw9-date"
+                }
+              >
+                {
+                  stringDate
+                }
+              </span>
             </>
           ) : (
             <>
@@ -82,11 +240,41 @@ function Clock() {
         </div>
       </div>
 
-      <div className={s.buttonsContainer}>
-        <SuperButton id={"hw9-button-start"} disabled={disable} onClick={start} xType={"default"}>
+      <div
+        className={
+          s.buttonsContainer
+        }
+      >
+        <SuperButton
+          id={
+            "hw9-button-start"
+          }
+          disabled={
+            disable
+          }
+          onClick={
+            start
+          }
+          xType={
+            "default"
+          }
+        >
           start
         </SuperButton>
-        <SuperButton id={"hw9-button-stop"} disabled={!disable} onClick={stop} xType={"default"}>
+        <SuperButton
+          id={
+            "hw9-button-stop"
+          }
+          disabled={
+            !disable
+          }
+          onClick={
+            stop
+          }
+          xType={
+            "default"
+          }
+        >
           stop
         </SuperButton>
       </div>
