a36e8145bb366eddb5a01eff3de573ee5f61be43
alexMuhin <myx87@bk.ru>
2024-09-30 16:18:16 +0300

    refactoring project

--- src/s2-homeworks/hw05/header/Header.tsx
+++ src/s2-homeworks/hw05/header/Header.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from "react"
+import React, {
+  FC,
+} from "react"
 import burgerIcon from "./burger.svg"
 import s from "./Header.module.css"
 import { useLocation } from "react-router-dom"
@@ -8,29 +10,57 @@ type PropsType = {
   handleOpen: () => void
 }
 
-export const Header: FC<PropsType> = ({ handleOpen }) => {
-  const location = useLocation()
-  const currentPath = location.pathname
+export const Header: FC<
+  PropsType
+> = ({
+  handleOpen,
+}) => {
+  const location =
+    useLocation()
+  const currentPath =
+    location.pathname
 
   const pageName =
-    currentPath === PATH.PRE_JUNIOR
+    currentPath ===
+    PATH.PRE_JUNIOR
       ? "Pre-junior"
-      : currentPath === PATH.JUNIOR
+      : currentPath ===
+          PATH.JUNIOR
         ? "Junior"
-        : currentPath === PATH.JUNIOR_PLUS
+        : currentPath ===
+            PATH.JUNIOR_PLUS
           ? "Junior Plus"
           : "Error"
   return (
     <>
-      <div id={"hw5-header"} className={s.header}>
+      <div
+        id={
+          "hw5-header"
+        }
+        className={
+          s.header
+        }
+      >
         <img
-          src={burgerIcon}
-          id={"hw5-burger-menu"}
-          className={s.burgerMenuIcon}
-          onClick={handleOpen}
-          alt={"open menu"}
+          src={
+            burgerIcon
+          }
+          id={
+            "hw5-burger-menu"
+          }
+          className={
+            s.burgerMenuIcon
+          }
+          onClick={
+            handleOpen
+          }
+          alt={
+            "open menu"
+          }
         />
-        <h1>{pageName}</h1>
+        <h1>
+          {pageName}
+        </h1>
       </div>
     </>
   )
