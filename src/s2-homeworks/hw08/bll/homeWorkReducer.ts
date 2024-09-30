fa560590976d53a3526666a3368eb3fed11871d3
alexMuhin <myx87@bk.ru>
2024-09-30 16:18:20 +0300

    refactoring project

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,29 +1,61 @@
-import { initialPeople, UserType } from "../HW8"
+import {
+  initialPeople,
+  UserType,
+} from "../HW8"
 
 type ActionType =
   | {
       type: "sort"
-      payload: "up" | "down"
+      payload:
+        | "up"
+        | "down"
     }
   | {
       type: "check"
       payload: number
     }
 
-export const homeWorkReducer = (state: UserType[] = initialPeople, action: ActionType): UserType[] => {
-  switch (action.type) {
-    case "sort":
-      return [...state].sort((a, b) => {
-        if (action.payload === "up") {
-          return a.name.localeCompare(b.name)
-        } else if (action.payload === "down") {
-          return b.name.localeCompare(a.name)
-        }
-        return 0
-      })
-    case "check":
-      return state.filter((u) => u.age >= action.payload)
-    default:
-      return state
+export const homeWorkReducer =
+  (
+    state: UserType[] = initialPeople,
+    action: ActionType,
+  ): UserType[] => {
+    switch (
+      action.type
+    ) {
+      case "sort":
+        return [
+          ...state,
+        ].sort(
+          (
+            a,
+            b,
+          ) => {
+            if (
+              action.payload ===
+              "up"
+            ) {
+              return a.name.localeCompare(
+                b.name,
+              )
+            } else if (
+              action.payload ===
+              "down"
+            ) {
+              return b.name.localeCompare(
+                a.name,
+              )
+            }
+            return 0
+          },
+        )
+      case "check":
+        return state.filter(
+          (u) =>
+            u.age >=
+            action.payload,
+        )
+      default:
+        return state
+    }
   }
-}
