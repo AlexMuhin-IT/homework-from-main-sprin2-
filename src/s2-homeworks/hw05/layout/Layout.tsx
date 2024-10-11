0ed41697b75bcaaff29289ae41d8511be4f9c54b
alexMuhin <myx87@bk.ru>
2024-10-11 16:19:39 +0300

    refactoring project

--- src/s2-homeworks/hw05/layout/Layout.tsx
+++ src/s2-homeworks/hw05/layout/Layout.tsx
@@ -1,4 +1,9 @@
-import React, { FC, ReactNode, useEffect, useState } from "react"
+import React, {
+  FC,
+  ReactNode,
+  useEffect,
+  useState,
+} from "react"
 import { Header } from "../header/Header"
 import { Sidebar } from "../sidebar/Sidebar"
 
@@ -6,21 +11,48 @@ type PropsType = {
   children: ReactNode
 }
 
-export const Layout: FC<PropsType> = ({ children }) => {
-  const [open, setOpen] = useState(false)
-  const handleClose = () => setOpen(false)
-  const handleOpen = () => setOpen(true)
+export const Layout: FC<
+  PropsType
+> = ({
+  children,
+}) => {
+  const [
+    open,
+    setOpen,
+  ] =
+    useState(false)
+  const handleClose =
+    () =>
+      setOpen(false)
+  const handleOpen =
+    () =>
+      setOpen(true)
 
   useEffect(() => {
-    open && (document.body.style.overflow = "hidden")
-    !open && (document.body.style.overflow = "unset")
+    open &&
+      (document.body.style.overflow =
+        "hidden")
+    !open &&
+      (document.body.style.overflow =
+        "unset")
   }, [open])
 
   return (
     <>
-      <Sidebar open={open} handleClose={handleClose} />
-      <Header handleOpen={handleOpen} />
-      <div>{children}</div>
+      <Sidebar
+        open={open}
+        handleClose={
+          handleClose
+        }
+      />
+      <Header
+        handleOpen={
+          handleOpen
+        }
+      />
+      <div>
+        {children}
+      </div>
     </>
   )
 }
