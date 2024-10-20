d5339c954e0fedf0b3cd6b5ac1c39fb874f2144b
alexMuhin <myx87@bk.ru>
2024-10-20 18:33:18 +0300

    homework finish

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -16,34 +16,18 @@ export const PATH = {
 function Pages() {
     return (
         <div>
-            {/*Routes выбирает первый подходящий роут*/}
-            {/*<div><h1>Header</h1></div>*/}
-            {/*<div>*/}
-                {/*<NavLink to={PATH.PRE_JUNIOR}>PreJunior</NavLink>*/}
-                {/*<NavLink to={PATH.JUNIOR}>Junior</NavLink>*/}
-                {/*<NavLink to={PATH.JUNIOR_PLUS}>JuniorPlus</NavLink>*/}
-            {/*</div>*/}
             <div>
                 <Routes>
-                    {/*роутинг будут писать студенты*/}
-                    {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                     <Route path='/' element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
-
-                    {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                    {/*<Routes>*/}
                         <Route path='/' element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
 
                         <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                         <Route path={PATH.JUNIOR} element={<Junior/>}/>
                         <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
-                        {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                        {/*<Route path={PATH.ERROR404} element={<Error404/>}/>*/}
                         <Route path='*' element={<Error404/>}/>
-                    {/*</Routes>*/}
                 </Routes>
             </div>
-
         </div>
     )
 }
