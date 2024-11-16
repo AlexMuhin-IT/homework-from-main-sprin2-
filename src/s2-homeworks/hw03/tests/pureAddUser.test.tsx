91a1eab7166aff9017909ed53cd1ae7308fe193c
alexMuhin <myx87@bk.ru>
2024-11-16 16:28:00 +0300

    refactoring project

--- src/s2-homeworks/hw03/tests/pureAddUser.test.tsx
+++ src/s2-homeworks/hw03/tests/pureAddUser.test.tsx
@@ -2,22 +2,17 @@ import React from "react"
 import { pureAddUser } from "../GreetingContainer"
 
 let name: any
-const setName = (
-  a: any,
-) => {
+const setName = (a: any) => {
   name = a
 }
 let error: any
-const setError = (
-  a: any,
-) => {
+const setError = (a: any) => {
   error = a
 }
 let added: any
-const addUserCallback =
-  () => {
-    added = true
-  }
+const addUserCallback = () => {
+  added = true
+}
 
 beforeEach(() => {
   name = ""
@@ -27,59 +22,22 @@ beforeEach(() => {
 
 test("name 1", () => {
   name = "1"
-  pureAddUser(
-    name,
-    setError,
-    setName,
-    addUserCallback,
-  )
-  expect(name).toBe(
-    "",
-  )
-  expect(
-    error,
-  ).toBe("")
-  expect(
-    added,
-  ).toBe(true)
+  pureAddUser(name, setError, setName, addUserCallback)
+  expect(name).toBe("")
+  expect(error).toBe("")
+  expect(added).toBe(true)
 })
 test("name 2", () => {
   name = ""
-  pureAddUser(
-    name,
-    setError,
-    setName,
-    addUserCallback,
-  )
-  expect(name).toBe(
-    "",
-  )
-  expect(
-    error,
-  ).toBe(
-    "Ошибка! Введите имя!",
-  )
-  expect(
-    added,
-  ).toBe(false)
+  pureAddUser(name, setError, setName, addUserCallback)
+  expect(name).toBe("")
+  expect(error).toBe("Ошибка! Введите имя!")
+  expect(added).toBe(false)
 })
 test("name 3", () => {
   name = "    "
-  pureAddUser(
-    name,
-    setError,
-    setName,
-    addUserCallback,
-  )
-  expect(name).toBe(
-    "    ",
-  )
-  expect(
-    error,
-  ).toBe(
-    "Ошибка! Введите имя!",
-  )
-  expect(
-    added,
-  ).toBe(false)
+  pureAddUser(name, setError, setName, addUserCallback)
+  expect(name).toBe("    ")
+  expect(error).toBe("Ошибка! Введите имя!")
+  expect(added).toBe(false)
 })
