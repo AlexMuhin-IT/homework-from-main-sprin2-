c334e9152a32e9252c730da6738c69614f2d469e
alexMuhin <myx87@bk.ru>
2024-11-18 16:28:20 +0300

    refactoring project

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -1,6 +1,4 @@
-import React, {
-  FC,
-} from "react"
+import React, { FC } from "react"
 import { NavLink } from "react-router-dom"
 import s from "./Sidebar.module.css"
 import { PATH } from "../Pages"
@@ -11,124 +9,43 @@ type PropsType = {
   handleClose: () => void
 }
 
-export const Sidebar: FC<
-  PropsType
-> = ({
-  open,
-  handleClose,
-}) => {
-  const sidebarClass =
-    s.sidebar +
-    (open
-      ? " " + s.open
-      : "")
+export const Sidebar: FC<PropsType> = ({ open, handleClose }) => {
+  const sidebarClass = s.sidebar + (open ? " " + s.open : "")
 
   return (
     <>
       {/*затемнение справа от открытого меню*/}
-      {open && (
-        <div
-          className={
-            s.background
-          }
-          onClick={
-            handleClose
-          }
-        />
-      )}
+      {open && <div className={s.background} onClick={handleClose} />}
 
-      <aside
-        className={
-          sidebarClass
-        }
-      >
-        <button
-          className={
-            s.close
-          }
-          onClick={
-            handleClose
-          }
-        >
-          <img
-            src={
-              closeIcon
-            }
-            alt="close sidebar"
-            id={
-              "hw5-menu-close"
-            }
-          />
+      <aside className={sidebarClass}>
+        <button className={s.close} onClick={handleClose}>
+          <img src={closeIcon} alt="close sidebar" id={"hw5-menu-close"} />
         </button>
 
-        <nav
-          id={
-            "hw5-menu"
-          }
-          className={
-            s.nav
-          }
-        >
+        <nav id={"hw5-menu"} className={s.nav}>
           <NavLink
-            id={
-              "hw5-pre-junior-link"
-            }
-            to={
-              PATH.PRE_JUNIOR
-            }
-            onClick={
-              handleClose
-            }
-            className={({
-              isActive,
-            }) =>
-              isActive
-                ? `${s.nav} ${s.active}`
-                : s.nav
-            }
+            id={"hw5-pre-junior-link"}
+            to={PATH.PRE_JUNIOR}
+            onClick={handleClose}
+            className={({ isActive }) => (isActive ? `${s.nav} ${s.active}` : s.nav)}
           >
             Pre-junior
           </NavLink>
           <NavLink
-            id={
-              "hw5-junior-link"
-            }
-            to={
-              PATH.JUNIOR
-            }
-            onClick={
-              handleClose
-            }
-            className={({
-              isActive,
-            }) =>
-              isActive
-                ? `${s.nav} ${s.active}`
-                : s.nav
-            }
+            id={"hw5-junior-link"}
+            to={PATH.JUNIOR}
+            onClick={handleClose}
+            className={({ isActive }) => (isActive ? `${s.nav} ${s.active}` : s.nav)}
           >
             Junior
           </NavLink>
           <NavLink
-            id={
-              "hw5-junior-plus-link"
-            }
-            to={
-              PATH.JUNIOR_PLUS
-            }
-            onClick={
-              handleClose
-            }
-            className={({
-              isActive,
-            }) =>
-              isActive
-                ? `${s.nav} ${s.active}`
-                : s.nav
-            }
+            id={"hw5-junior-plus-link"}
+            to={PATH.JUNIOR_PLUS}
+            onClick={handleClose}
+            className={({ isActive }) => (isActive ? `${s.nav} ${s.active}` : s.nav)}
           >
-            Junior
-            Plus
+            Junior Plus
           </NavLink>
         </nav>
       </aside>
