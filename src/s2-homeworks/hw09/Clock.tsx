037b850bc945043eae2e0f7e6ccb3b404196b2b0
alexMuhin <myx87@bk.ru>
2024-11-20 00:06:12 +0300

    homework HW9 in processes

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -8,24 +8,38 @@ function Clock() {
 	const [timerId, setTimerId] = useState<number | undefined>(undefined)
 	// for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
 	const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
-	const [show, setShow] = useState<boolean>(true)
+	const [show, setShow] = useState<boolean>(false)
+
+	const [disable, setDisable] = useState<boolean>(false) // ????
+	// правильно ли создать новый стать?! или объясните пожалуйста
 
 	const start = () => {
 		// пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
 		// сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-		setShow(true)
+		// setShow(true)
+		stop()
+		setDisable(!disable)
+		const id: number = window.setInterval(() => {
+
+			setDate(new Date())
+			setTimerId(id)
+		}, 1000)
+		// setShow(true)
+		// setTimerId(id)
 	}
 
 	const stop = () => {
 		// пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
-
+		clearInterval(timerId)
+		// setShow(!show)
+		setDisable(!disable)
 	}
 
 	const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
-
+		setShow(true)
 	}
 	const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
-
+		setShow(false)
 	}
 	let formatterTime = new Intl.DateTimeFormat("ru", {
 		hour: "numeric",
@@ -37,22 +51,18 @@ function Clock() {
 	let formatterDate = new Intl.DateTimeFormat("ru");
 	let currentDate = formatterDate.format(date)
 
-	let formatterMonth = new Intl.DateTimeFormat("en-US", {
-		month: "long",
-	});
+	let formatterMonth = new Intl.DateTimeFormat("en-US", {month: "long",});
 	let currentMonth = formatterMonth.format(date)
 
-	let formatterDay = new Intl.DateTimeFormat("en-US", {
-		weekday: "long"
-	});
+	let formatterDay = new Intl.DateTimeFormat("en-US", {weekday: "long"});
 	let currentDay = formatterDay.format(date)
 
 	const stringTime = currentTime || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-	const stringDate = `(${currentDate})` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+	const stringDate = currentDate || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
 	// день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
 	const stringDay = currentDay || <br/> // пишут студенты
-	const stringMonth = `${currentMonth}` || <br/> // пишут студенты
+	const stringMonth = currentMonth || <br/> // пишут студенты
 
 	return (
 		<div className={s.clock}>
@@ -64,7 +74,7 @@ function Clock() {
 			>
 				<span id={'hw9-day'}>{stringDay}</span>,{' '}
 				<span id={'hw9-time'}>
-                    <strong>{stringTime}</strong>
+                    <strong onBlur={event => onMouseEnter()}>{stringTime}</strong>
                 </span>
 			</div>
 
@@ -86,17 +96,19 @@ function Clock() {
 			<div className={s.buttonsContainer}>
 				<SuperButton
 					id={'hw9-button-start'}
-					disabled={show} // пишут студенты // задизэйблить если таймер запущен
+					disabled={disable} // пишут студенты // задизэйблить если таймер запущен
 					onClick={start}
-					className={'secondary'}
+					xType={'default'}
+
 				>
 					start
 				</SuperButton>
 				<SuperButton
 					id={'hw9-button-stop'}
-					disabled={show} // пишут студенты // задизэйблить если таймер не запущен
+					disabled={!disable} // пишут студенты // задизэйблить если таймер не запущен
 					onClick={stop}
-					className={'secondary'}
+					xType={'default'}
+
 				>
 					stop
 				</SuperButton>
