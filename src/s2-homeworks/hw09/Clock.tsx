4fed55717dd1d624e30cd85bb8ebb1dcc24a06cf
alexMuhin <myx87@bk.ru>
2024-11-20 00:06:35 +0300

    homework HW9 in processes

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -61,7 +61,6 @@ function Clock() {
 	const stringDate = currentDate || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
 	// день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-
 	const stringDay = currentDay || <br/> // пишут студенты
 	const stringMonth = currentMonth || <br/> // пишут студенты
 
