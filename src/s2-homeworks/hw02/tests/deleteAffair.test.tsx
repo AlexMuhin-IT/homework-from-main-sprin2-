234c525984e8fd443a9e3e7a8b408a646fdeed6c
alexMuhin <myx87@bk.ru>
2024-11-22 16:24:40 +0300

    refactoring project

--- src/s2-homeworks/hw02/tests/deleteAffair.test.tsx
+++ src/s2-homeworks/hw02/tests/deleteAffair.test.tsx
@@ -1,8 +1,5 @@
 import React from "react"
-import {
-  AffairType,
-  deleteAffair,
-} from "../HW2"
+import { AffairType, deleteAffair } from "../HW2"
 
 let initialState: AffairType[]
 
@@ -11,83 +8,48 @@ beforeEach(() => {
     {
       _id: 1,
       name: "React",
-      priority:
-        "high",
+      priority: "high",
     },
     {
       _id: 2,
       name: "anime",
-      priority:
-        "low",
+      priority: "low",
     },
     {
       _id: 3,
       name: "games",
-      priority:
-        "low",
+      priority: "low",
     },
     {
       _id: 4,
       name: "work",
-      priority:
-        "high",
+      priority: "high",
     },
     {
       _id: 5,
       name: "html & css",
-      priority:
-        "middle",
+      priority: "middle",
     },
   ]
 })
 
 test("delete 0", () => {
-  const newState =
-    deleteAffair(
-      initialState,
-      0,
-    )
-  expect(
-    newState.length,
-  ).toBe(5)
+  const newState = deleteAffair(initialState, 0)
+  expect(newState.length).toBe(5)
 })
 test("delete 1", () => {
-  const newState =
-    deleteAffair(
-      initialState,
-      1,
-    )
-  expect(
-    newState.length,
-  ).toBe(4)
+  const newState = deleteAffair(initialState, 1)
+  expect(newState.length).toBe(4)
 })
 test("delete 3", () => {
-  const newState =
-    deleteAffair(
-      initialState,
-      3,
-    )
-  expect(
-    newState.length,
-  ).toBe(4)
+  const newState = deleteAffair(initialState, 3)
+  expect(newState.length).toBe(4)
 })
 test("delete 5", () => {
-  const newState =
-    deleteAffair(
-      initialState,
-      5,
-    )
-  expect(
-    newState.length,
-  ).toBe(4)
+  const newState = deleteAffair(initialState, 5)
+  expect(newState.length).toBe(4)
 })
 test("delete 6", () => {
-  const newState =
-    deleteAffair(
-      initialState,
-      6,
-    )
-  expect(
-    newState.length,
-  ).toBe(5)
+  const newState = deleteAffair(initialState, 6)
+  expect(newState.length).toBe(5)
 })
