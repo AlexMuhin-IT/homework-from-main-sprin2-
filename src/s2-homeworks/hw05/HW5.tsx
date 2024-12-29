9de12f824d9b22ee62e56ddb090f48aaf19a5171
alexMuhin <myx87@bk.ru>
2024-12-29 21:12:34 +0300

    homework HW15 финиш

--- src/s2-homeworks/hw05/HW5.tsx
+++ src/s2-homeworks/hw05/HW5.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import { HashRouter } from 'react-router-dom'
-import { Layout } from './layout/Layout'
+import {HashRouter} from 'react-router-dom'
+import {Layout} from './layout/Layout'
 import Pages from './Pages'
+import HW14 from "../hw14/HW14";
 
 function HW5() {
-    return (
-        <HashRouter>
-            <Layout>
-                <Pages />
-            </Layout>
-        </HashRouter>
-    )
+	return (
+		<HashRouter>
+			<Layout>
+				<Pages/>
+			</Layout>
+		</HashRouter>
+	)
 }
 
 export default HW5
